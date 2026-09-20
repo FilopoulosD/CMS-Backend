c99ec5d24bda4a062e48b64b55eb3eb540a2c322
Filopoulos Dimitrios <filopoulosdim@gmail.com>
2026-09-20 20:56:24 +0300

    update: umbrella domain to server.js

--- server.js
+++ server.js
@@ -43,7 +43,8 @@ app.get('/', (req, res) => {
 });
 
 const PORT = process.env.PORT || 5000;
+const UMBRELLA = process.env.UMBRELLA_DOMAIN || localhost;
 
 app.listen(PORT, () => {
-    console.log(`API running on http://localhost:${PORT}`);
+    console.log(`API running on http://${UMBRELLA}:${PORT}`);
 });
